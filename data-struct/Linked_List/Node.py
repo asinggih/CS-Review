a9029d1fca79dc16985d0e9376cae1beb1591a32
Aditya Singgih <adityasinggih@msn.com>
2019-03-25 23:36:48 +1100

    created node class

---
+++ data-struct/Linked_List/Node.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class Node:
+    """Node class to be used with Linked List"""
+
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next = next_node
