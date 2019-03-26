f2399741b8c80818577be1048ee6035cbc15e141
Aditya Singgih <adityasinggih@msn.com>
2019-03-26 12:18:49 +1100

    added test data to LinkedList

--- data-struct/Linked_List/test_linked_list.py
+++ data-struct/Linked_List/test_linked_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import pytest
 from Node import Node
 from LinkedList import LinkedList
 
@@ -103,3 +104,33 @@ def test_delete_multiple():
     ll.delete(30)
     ll.delete(12)
     assert ll.display() == "20"
+
+
+def test_delete_duplicates():
+
+    blob = [12, 30, 43, 12, 50, 30]
+    ll = LinkedList()
+    for i in blob:
+        ll.append(Node(i))
+    ll.delete(30)
+    assert "30" in ll.display()
+
+
+def test_delete_at_index():
+
+    blob = [12, 30, 43, 13, 50, 30]
+    ll = LinkedList()
+    for i in blob:
+        ll.append(Node(i))
+    ll.delete_at_index(0)
+    print(ll.display())
+    assert "12" not in ll.display()
+
+
+def test_delete_at_index_out_of_range():
+    ll = LinkedList(Node(20))
+    with pytest.raises(IndexError) as e:
+        # ll.delete_at_index(1)
+        ll.delete_at_index(-1)
+    assert str(
+        e.value) == "Index has to be >= 0 and Index < than Linked List's size"
